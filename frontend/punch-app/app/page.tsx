c129ca4b13a4bf0bf56f7cf6e5b63fb962739e8d
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-03-23 15:13:33 +0300

    set up api url and usestate frontend

---
+++ frontend/punch-app/app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+const API_URL = "http://localhost:5000/api/posts/";
+
+import Image from "next/image";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [newPosts, setNewPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("Error fetching posts", error);
+    }
+  };
+  return (
+    <div className="flex flex-col bg-gray-100 min-h-screen rounded-md  m-4 p-4 font-sans">
+      <h1 className="text-blue-500 flex justify-center  ">
+        Welcome to Punch Group
+      </h1>
+    </div>
+  );
+}
